b35e58112f73c0c32d1896701e1604b896afb7b8
zactony <zactony@outlook.com>
2020-12-22 07:03:53 +0800

    feat: 新增有序链表数据结构

---
+++ src/LinkedList/sortedLinkedList.js
@@ -0,0 +1,58 @@
+import LinkedList from './linkedList.js';
+import { isFunction } from '../utils.js';
+
+/**
+ * 数据结构 - 有序链表
+ * @extends LinkedList
+ */
+class SortedLinkedList extends LinkedList {
+  /**
+   * 排序函数
+   * @protected
+   */
+  sortFn = () => {}
+
+  constructor(equalsFn, sortFn = (a, b) => a - b) {
+    super(equalsFn);
+    if (!isFunction(sortFn)) throw new TypeError(`${sortFn} isn't function`)
+    this.sortFn = sortFn;
+  }
+
+  /**
+   * 插入节点
+   * @public
+   * @override
+   * @param {any} element 待插入值
+   * @returns {Object} 插入新值后的链表
+   */
+  insert(element) {
+    if (this.isEmpty()) {
+      return super.insert(element, 0);
+    }
+    const index = this.#getIndexNextSortedElement(element);
+    return super.insert(element, index);
+  }
+
+  /**
+   * 根据待插入的值和排序函数找到插入的指针
+   * @private
+   * @param {any} element 待排序的值
+   * @returns {number} 指针
+   */
+  #getIndexNextSortedElement(element) {
+    let current = this.head;
+    let index = 0;
+
+    while(!current.next) {
+      index += 1;
+      if (this.sortFn(element, current.value) <= 0) {
+        break;
+      } 
+      current = current.next;
+    }
+
+    return index;
+  }
+}
+
+export default SortedLinkedList;
