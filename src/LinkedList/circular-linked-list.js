0e7e2e78aa4fc61254aa4a9c3c97600ef0474184
zactony <zactony@outlook.com>
2020-12-26 09:29:51 +0800

    docs: 新增逻辑注释说明

--- src/LinkedList/circular-linked-list.js
+++ src/LinkedList/circular-linked-list.js
@@ -21,13 +21,22 @@ class CircularLinkedList extends LinkedList {
     if (index < 0 || index > this.size()) return false;
     const node = new Node(element);
 
+    /**
+     * 第一种情况：如果指针为零，且链表为空，则直接添加链表做好头尾相接即可
+     * 第二种情况：如果指针为零，且链表不为空，首先获取到当前链表的尾部节点，
+     *            然后将下级索引链接到新节点，接着新节点的下级索引链接到现
+     *            有链表，然后更新存储的链表
+     * 第三种情况：如果指针不为零，则需要获取到待插入位置的前一位，将新节点的
+     *            下级索引链接到前者的下级索引，然后将前者的下级索引链接到新节点
+     */
+
     if (!index) {
       if (this.isEmpty()) {
         this.head = node;
         node.next = this.head;
       } else {
-        node.next = this.head;
         const tail = this.getElementAt(this.size() - 1);
+        node.next = this.head;
         tail.next = node;
         this.head = node;
       }
@@ -54,6 +63,15 @@ class CircularLinkedList extends LinkedList {
 
     if (index < 0 || index >= this.size()) return false;
     let current = this.head;
+
+    /**
+     * 第一种情况：如果指针为零，且链表长度为1，则直接清空当前链表
+     * 第二种情况：如果指针为零，且链表长度大于1，首先获取到当前链表
+     *            的尾部节点，然后链表更新头部节点，尾部节点链接到
+     *            新的头部节点
+     * 第三种情况：如果指针不为零，首先获取到待移除位置的前序节点，然后
+     *            将前序节点的下级索引链接到待移除位置的下级索引
+     */
 
     if (!index) {
       if (this.size() === 1) {
