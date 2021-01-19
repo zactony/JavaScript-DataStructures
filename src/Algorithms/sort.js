94b7bfa3030a1b824af5b0ab5fdbfaabb673d459
zactony <zactony@outlook.com>
2021-01-20 07:51:36 +0800

    feat: 选择排序和插入排序新增

--- src/Algorithms/sort.js
+++ src/Algorithms/sort.js
@@ -29,4 +29,54 @@ export const modifiedBubbleSort = (array) => {
   }
 };
 
-export const a = () => {};
+/**
+ * 选择排序每次循环找到当前最小值，然后放在数组的前端
+ *
+ * @param array {Array} 待排序数组
+ */
+export const selectionSort = (array) => {
+  let minValueIndex;
+
+  for (let i = 0; i < array.length - 1; i += 1) {
+    minValueIndex = i;
+    for (let j = i; j < array.length; j += 1) {
+      if (array[minValueIndex] > array[j]) {
+        minValueIndex = j;
+      }
+    }
+
+    if (i !== minValueIndex) {
+      [array[minValueIndex], array[i]] = [array[i], array[minValueIndex]];
+    }
+  }
+};
+
+/**
+ * 插入排序每次循环拿当前的值和之前的值依次比较大小，然后互相交换直到结束
+ *
+ * @param array {Array} 待排序数组
+ */
+export const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i += 1) {
+    let j = i;
+    const temp = array[i];
+
+    while (j > 0 && array[j - 1] > temp) {
+      array[j] = array[j - 1];
+      j -= 1;
+    }
+
+    array[j] = temp;
+  }
+};
+
+export const insertionSortOther = (array) => {
+  for (let i = 1; i < array.length; i += 1) {
+    let j = i;
+
+    while (j > 0 && array[j - 1] > array[j]) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
+      j -= 1;
+    }
+  }
+};
