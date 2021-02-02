34c72af575bb36c4a9d23a845b4780859253363f
zactony <zactony@outlook.com>
2021-02-02 20:13:22 +0800

    feat: 基数排序新增

--- src/Algorithms/sort.js
+++ src/Algorithms/sort.js
@@ -220,3 +220,50 @@ export const bucketSort = (array, bucketSize = 5) => {
   const buckets = createBuckets(array, bucketSize);
   return sortBuckets(buckets);
 };
+
+/**
+ * 基数排序
+ */
+const countingSortForRadix = (array, radixBase, significantDigit, minValue) => {
+  let bucketsIndex;
+  const buckets = [];
+  const aux = [];
+
+  for (let i = 0; i < radixBase; i++) {
+    buckets[i] = 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    bucketsIndex = Math.floor(((array[i] - minValue) / significantDigit) % radixBase);
+    buckets[bucketsIndex]++;
+  }
+
+  for (let i = 1; i < radixBase; i++) {
+    buckets[i] += buckets[i - 1];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    bucketsIndex = Math.floor(((array[i] - minValue) / significantDigit) % radixBase);
+    aux[--buckets[bucketsIndex]] = array[i];
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    array[i] = aux[i];
+  }
+
+  return array;
+};
+
+export const radixSort = (array, radixBase = 10) => {
+  if (array.length < 2) return array;
+  const minValue = Math.min(array);
+  const maxValue = Math.max(array);
+
+  let significantDigit = 1;
+  while ((maxValue - minValue) / significantDigit >= 1) {
+    array = countingSortForRadix(array, radixBase, significantDigit, minValue);
+    significantDigit *= radixBase;
+  }
+
+  return array;
+};
